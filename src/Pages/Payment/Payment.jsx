ee0bf36f60078b4448203b181c5798df9db134e9
SHAHAB UDDIN <shahab.82946@gmail.com>
2025-03-11 23:13:11 +0600

    payment component created

---
+++ src/Pages/Payment/Payment.jsx
@@ -0,0 +1,16 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe('')
+const Payment = () => {
+        return (
+                <div>
+                <Elements stripe={stripePromise}>
+                        
+                </Elements>
+                        
+                </div>
+        );
+};
+
+export default Payment;
