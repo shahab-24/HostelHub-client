3f18f6fa5820d44dd09c5b8ccfdeab4dac97ebcf
SHAHAB UDDIN <shahab.82946@gmail.com>
2025-03-12 22:34:19 +0600

    checkout page customize

--- src/Pages/Payment/CheckoutForm.jsx
+++ src/Pages/Payment/CheckoutForm.jsx
@@ -1,49 +1,46 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 
 const CheckoutForm = () => {
-        const stripe = useStripe()
-        const elements = useElements()
+  const stripe = useStripe();
+  const elements = useElements();
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        const handleSubmit = e => {
-                e.preventDefault()
+    if (!stripe || !elements) {
+      return;
+    }
 
-                if(!stripe || !elements){
-                        return
-                }
-
-                const card = elements.getElement(CardElement)
-                if(card == null){
-                        return
-                }
-        }
-        return (
-                <div>
-                <form onSubmit={handleSubmit}>
-                <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: '16px',
-              color: '#424770',
-              '::placeholder': {
-                color: '#aab7c4',
+    const card = elements.getElement(CardElement);
+    if (card == null) {
+      return;
+    }
+  };
+  return (
+    <div className="my-20 flex justify-center items-center border-orange-400 border-2 w-1/2">
+      <form onSubmit={handleSubmit}>
+        <CardElement className="w-[500px] py-2 my-2 border-blue-400 border-2 "
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
               },
             },
-            invalid: {
-              color: '#9e2146',
-            },
-          },
-        }}
-      />
-      <button type="submit" disabled={!stripe}>
-        Pay
-      </button>
-
-                </form>
-                        
-                </div>
-        );
+          }}
+        />
+        <button className="bg-green-400 w-full text-white px-4 py-2 rounded-md gap-10 font-semibold" type="submit" disabled={!stripe}>
+          Pay
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default CheckoutForm;
--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -5,7 +5,7 @@ import CheckoutForm from "./CheckoutForm";
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PK)
 const Payment = () => {
         return (
-                <div>
+                <div className="flex justify-center items-center gap-20 border-red-400 border-2">
                 <Elements stripe={stripePromise}>
                         <CheckoutForm></CheckoutForm>
                 </Elements>
