360cd63094ed7422e70d5f8d4374ef37079a7abe
SHAHAB UDDIN <shahab.82946@gmail.com>
2025-03-13 00:25:58 +0600

    user state use to show error

--- src/Pages/Payment/CheckoutForm.jsx
+++ src/Pages/Payment/CheckoutForm.jsx
@@ -1,10 +1,12 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 const CheckoutForm = () => {
+        const [error, setError] = useState("")
   const stripe = useStripe();
   const elements = useElements();
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (!stripe || !elements) {
@@ -15,21 +17,24 @@ const CheckoutForm = () => {
     if (card == null) {
       return;
     }
-    const {error, paymentMethod} = await stripe.createPaymentMethod({
-        type: 'card',
-        card
 
-    })
-    if(error){
-        console.log('payment error', error)
-    }else{
-        console.log('payment method', paymentMethod)
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+    if (error) {
+      console.log("payment error", error);
+      setError(error.message)
+    } else {
+      console.log("payment method", paymentMethod);
+      setError('')
     }
   };
   return (
     <div className="my-20 flex justify-center items-center border-orange-400 border-2 w-1/2">
       <form onSubmit={handleSubmit}>
-        <CardElement className="w-[500px] py-2 my-2 border-blue-400 border-2 "
+        <CardElement
+          className="w-[500px] py-2 my-2 border-blue-400 border-2 "
           options={{
             style: {
               base: {
@@ -45,9 +50,14 @@ const CheckoutForm = () => {
             },
           }}
         />
-        <button className="bg-green-400 w-full text-white px-4 py-2 rounded-md gap-10 font-semibold" type="submit" disabled={!stripe}>
+        <button
+          className="bg-green-400 w-full text-white px-4 py-2 rounded-md font-semibold"
+          type="submit"
+          disabled={!stripe}
+        >
           Pay
         </button>
+        <p className="text-red-400">{error}</p>
       </form>
     </div>
   );
